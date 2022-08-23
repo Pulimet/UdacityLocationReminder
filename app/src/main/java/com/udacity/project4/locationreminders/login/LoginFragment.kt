93a5ba11d9e897c11be5d945275bd2d0a5b83c83
Pulimet <korolev2009@gmail.com>
2022-08-23 14:09:22 +0300

    Added FragmentBinding.kt and fixed tests dependencies issues

--- app/src/main/java/com/udacity/project4/locationreminders/login/LoginFragment.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/login/LoginFragment.kt
@@ -2,8 +2,11 @@ package com.udacity.project4.locationreminders.login
 
 import androidx.fragment.app.Fragment
 import com.udacity.project4.R
+import com.udacity.project4.databinding.FragmentLoginBinding
+import com.udacity.project4.utils.FragmentBinding
 import org.koin.android.ext.android.inject
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
+    private val binding by FragmentBinding(FragmentLoginBinding::bind)
     private val viewModel: LoginViewModel by inject()
 }
