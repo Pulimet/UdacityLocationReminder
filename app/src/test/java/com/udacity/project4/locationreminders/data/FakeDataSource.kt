a742d0f1f75dd1da5c4783e9d011a640da3bf498
Pulimet <korolev2009@gmail.com>
2022-09-14 08:18:20 +0300

    Add RemindersListViewModelTest tests

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -1,15 +1,22 @@
 package com.udacity.project4.locationreminders.data
 
+import androidx.lifecycle.MutableLiveData
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
+import kotlinx.coroutines.runBlocking
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
 class FakeDataSource : ReminderDataSource {
+    private val observableRemindersTasks = MutableLiveData<Result<List<ReminderDTO>>>()
 
-//    TODO: Create a fake data source to act as a double to the real data source
+    fun addReminders(vararg reminders: ReminderDTO) {
+        runBlocking {
+            observableRemindersTasks.value = Result.Success(reminders.asList())
+        }
+    }
 
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        TODO("Return the reminders")
+        return observableRemindersTasks.value!!
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
--- app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,13 +1,54 @@
 package com.udacity.project4.locationreminders.reminderslist
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.extensions.getOrAwaitValue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
 class RemindersListViewModelTest {
+    private lateinit var remindersRepository: FakeDataSource
+    private lateinit var tasksViewModel: RemindersListViewModel
 
-    //TODO: provide testing to the RemindersListViewModel and its live data objects
+    private val rem1 = ReminderDTO("Title1", "Description1", "TA-1", 1.0, 1.0)
+    private val rem2 = ReminderDTO("Title2", "Description2", "TA-2", 2.0, 2.0)
+    private val rem3 = ReminderDTO("Title3", "Description3", "TA-3", 3.0, 3.0)
+
+
+    @Before
+    fun setupViewModel() {
+        remindersRepository = FakeDataSource()
+        remindersRepository.addReminders(rem1, rem2, rem3)
+        tasksViewModel = RemindersListViewModel(remindersRepository)
+    }
+
+    @Test
+    fun loadReminders() {
+        // WHEN
+        tasksViewModel.loadReminders()
+        // THEN
+        val value = tasksViewModel.remindersList.getOrAwaitValue()
+        Assert.assertEquals(value[0], covertDataFromDbToUIForm(rem1))
+        Assert.assertEquals(value[1], covertDataFromDbToUIForm(rem2))
+        Assert.assertEquals(value[2], covertDataFromDbToUIForm(rem3))
+    }
+
+    private fun covertDataFromDbToUIForm(reminder: ReminderDTO): ReminderDataItem {
+        return ReminderDataItem(
+            reminder.title,
+            reminder.description,
+            reminder.location,
+            reminder.latitude,
+            reminder.longitude,
+            reminder.id
+        )
+    }
+
 
 }
