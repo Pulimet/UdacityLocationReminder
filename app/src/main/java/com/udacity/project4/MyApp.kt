47ba7d12d8178823f52f0e1835b1ebcb0ff98311
Pulimet <korolev2009@gmail.com>
2022-09-18 05:10:26 +0300

    Add Code Review TODO's to MyApp.kt

--- app/src/main/java/com/udacity/project4/MyApp.kt
+++ app/src/main/java/com/udacity/project4/MyApp.kt
@@ -4,6 +4,17 @@ import android.app.Application
 
 class MyApp : Application() {
 
+    // Code review fixes
+    // TODO: The background location permission is requested in the select location screen (Nexus 5X, API 29).
+    // TODO: Device location and required permissions aren't checked or properly handled right before adding a geofence (Nexus 5X, API 29).
+    // TODO: All the users' reminders added earlier stop working after a certain amount of time.
+    // TODO: Coroutine is not properly tested using the check_loading testing function.
+    // TODO: Error handling is not properly done in the FakeDataSource class.
+    // TODO: No Espresso tests for Snackbar and Toast messages.
+    // TODO: Real DAO is not used in RemindersLocalRepositoryTest.
+    // TODO: Predictable errors like data not found are not tested.
+    // TODO: Idling resources are not properly used.
+
     override fun onCreate() {
         super.onCreate()
         Koin.init(applicationContext)
